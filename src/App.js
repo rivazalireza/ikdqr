872ca59e5617f96512298336eaed5e7189ed0b66
140107217 <140107217@IKD.ir>
2022-11-14 10:53:45 +0330

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
 
   render() {
       return (<div>
-          <h1>Html5Qrcode React example!</h1>
+          <h1>ali ikd qr</h1>
           <Html5QrcodePlugin 
               fps={10}
               qrbox={250}
