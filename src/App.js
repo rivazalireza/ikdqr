74dd3b9e8830509b686074c1d451382a9efe33f2
140107217 <140107217@IKD.ir>
2022-11-15 13:52:01 +0330

    update build for gh-pages

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () => {
   useEffect(() => {
     if (isAndroid) {
       const url =
-      "intent://qrcode-tiger.com/#Intent;scheme=https;package=com.qrtiger;end";
+      "intent:#Intent;scheme=https;package=tw.mobileapp.qrcode.banner;end";
 
       window.location.replace(url);
     } else if (isIOS) {
@@ -24,7 +24,7 @@ const App = () => {
   return (
     <div className="App">
       <div>
-    
+      tw.mobileapp.qrcode.banner
       </div>
       {isAndroid ? (
         <a href="https://play.google.com/store/apps/details?id=com.qrtiger&hl=en&gl=US">
