b72305a05eea023ff540db358f2297e36b4db42e
rivazalireza <118098566+rivazalireza@users.noreply.github.com>
2023-05-31 12:42:58 +0330

    Update App.js
    dd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import './App.css';
-
 import React from 'react';
-import Html5QrcodePlugin from './Html5QrcodePlugin.jsx'
-import ResultContainerPlugin from './ResultContainerPlugin.jsx'
-import HowToUse from './HowToUse.jsx'
 
 function App() {
   return (
